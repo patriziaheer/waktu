a246f08fb6571ed01bd63ecd164b2576da10fd80
Mike <mfisler@hsr.ch>
2011-04-12 17:32:06 +0200

    Custom Exception for illegal input

---
+++ WaktuPrototyp/src/ch/hsr/waktu/domain/InvalidInputException.java
@@ -0,0 +1,21 @@
+package ch.hsr.waktu.domain;
+
+public class InvalidInputException extends Throwable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String message = "";
+	
+	InvalidInputException() {
+		
+	}
+	
+	InvalidInputException(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+}
