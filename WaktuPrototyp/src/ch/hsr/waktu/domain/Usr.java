74e86ee8537019c4b6acc949b7896cbda4a90caa
Mike <mfisler@hsr.ch>
2011-04-12 17:30:15 +0200

    User updated, added missing attributes/setters/getters

--- WaktuPrototyp/src/ch/hsr/waktu/domain/Usr.java
+++ WaktuPrototyp/src/ch/hsr/waktu/domain/Usr.java
@@ -6,6 +6,10 @@ public class Usr {
 	private int id;
 	private String name;
 	private String firstname;
+	private String username;
+	private boolean inactive = false;
+	private int pensum;	
+	private int systemRole;
 	
 	public Usr() {
 		
@@ -34,6 +38,51 @@ public class Usr {
 	
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
+	}
+	
+	public int getPensum() {
+		return pensum;
+	}
+	
+	public void setPensum(int pensum) throws InvalidInputException {
+		if(pensum > 0 && pensum <= 100) {
+			this.pensum = pensum;
+		} else {
+			throw new InvalidInputException("Pensum muss gršsser 0%, maximal 100% sein");
+		}		
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+	
+	public int getSystemRole() {
+		return systemRole;
+	}
+	
+	public void setSystemRole(int systemRole) throws InvalidInputException {
+		if(systemRole > 0 && systemRole <= 2) {
+			this.systemRole = systemRole;
+		} else {
+			throw new InvalidInputException("Systemrollen: 0=Admin, 1=Projektleiter, 2=Projektmitarbeiter");
+		}
+		
+	}
+	
+	public boolean isInactive() {
+		return inactive;
+	}
+	
+	public void setInactive() {
+		this.inactive = true;
+	}
+	
+	public void setActive() {
+		this.inactive = false;
 	}
 	
 	@Override
