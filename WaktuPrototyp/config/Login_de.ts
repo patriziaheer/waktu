43b9bf3a4a1976789fa77498bebda3000946528e
unknown <pheer@.scs-ad.scs.ch>
2011-04-27 15:42:52 +0200

    TranslationFiles

---
+++ WaktuPrototyp/config/Login_de.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_CH">
+<context>
+    <name>LoginWindow</name>
+    <message>
+        <location filename="Ui_LoginWindow.java" line="78"/>
+        <source>Login Window</source>
+        <translation>Anmelden</translation>
+    </message>
+    <message>
+        <location filename="Ui_LoginWindow.java" line="79"/>
+        <source>Username:</source>
+        <translation>Benutzername:</translation>
+    </message>
+    <message>
+        <location filename="Ui_LoginWindow.java" line="80"/>
+        <source>Password:</source>
+        <translation>Passwort:</translation>
+    </message>
+    <message>
+        <location filename="Ui_LoginWindow.java" line="81"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="Ui_LoginWindow.java" line="82"/>
+        <source>Login</source>
+        <translation>Anmelden</translation>
+    </message>
+</context>
+</TS>
---
+++ WaktuPrototyp/config/Login_en.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>LoginWindow</name>
+    <message>
+        <location filename="Ui_LoginWindow.java" line="78"/>
+        <source>Login Window</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="Ui_LoginWindow.java" line="79"/>
+        <source>Username:</source>
+        <translation>Username:</translation>
+    </message>
+    <message>
+        <location filename="Ui_LoginWindow.java" line="80"/>
+        <source>Password:</source>
+        <translation>Password:</translation>
+    </message>
+    <message>
+        <location filename="Ui_LoginWindow.java" line="81"/>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <location filename="Ui_LoginWindow.java" line="82"/>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+</context>
+</TS>
