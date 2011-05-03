e90935b360787f18a3cd49ca9c193693a56312fd
Mike <mfisler@hsr.ch>
2011-05-03 16:04:41 +0200

    TimeUtil: test completed

--- WaktuPrototyp/test/ch/hsr/waktu/services/TestTimeUtil.java
+++ WaktuPrototyp/test/ch/hsr/waktu/services/TestTimeUtil.java
@@ -13,37 +13,72 @@ import com.trolltech.qt.core.QTime;
 public class TestTimeUtil {
 	@Test
 	public void convertGregorianToQDateTime_YearMonthDayHrsMinsSecs() {
-		assertEquals(new QDateTime(new QDate(2011, 4, 27), new QTime(15, 32, 23)), TimeUtil.convertGregorianToQDateTime(new GregorianCalendar(2011, 4, 27, 15, 32, 23)));
+		assertEquals(new QDateTime(new QDate(2011, 4, 27), new QTime(15, 32, 23)), 
+				TimeUtil.convertGregorianToQDateTime(
+						new GregorianCalendar(2011, 4, 27, 15, 32, 23)));
 	}
 	
 	@Test
 	public void convertQDateTimeToGregorian_YearMonthDayHrsMinsSecs() {
-		assertEquals(new GregorianCalendar(2011, 4, 27, 15, 32, 23), TimeUtil.convertQDateTimeToGregorian(new QDateTime(new QDate(2011, 4, 27), new QTime(15, 32, 23))));
+		assertEquals(new GregorianCalendar(2011, 4, 27, 15, 32, 23), 
+				TimeUtil.convertQDateTimeToGregorian(
+						new QDateTime(new QDate(2011, 4, 27), new QTime(15, 32, 23))));
 	}
 
 	@Test
-	public void calculateTimespanSecondsDifferenceNoOverflow() {
-		assertEquals(9, TimeUtil.calculateTimespanInSeconds(new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,32)), new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,41))));
+	public void calculateTimespanInSecondsYearMonthDayHrsMinsSecsDifference() {
+		assertEquals(9, TimeUtil.calculateTimespanInSeconds(
+				new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,32)), 
+				new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,41))));
 	}
 	
 	@Test
 	public void calculateTimespanMinutesDifferenceNoOverflow() {
-		assertEquals(42*60, TimeUtil.calculateTimespanInSeconds(new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,32)), new QDateTime(new QDate(2011, 3, 5), new QTime(12,45,32))));
+		assertEquals(42*60, TimeUtil.calculateTimespanInSeconds(
+				new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,32)), 
+				new QDateTime(new QDate(2011, 3, 5), new QTime(12,45,32))));
 	}
 	
 	@Test
 	public void calculateTimespanHourDifferenceNoOverflow() {
-		assertEquals(11*60*60, TimeUtil.calculateTimespanInSeconds(new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,32)), new QDateTime(new QDate(2011, 3, 5), new QTime(23,3,32))));
+		assertEquals(11*60*60, TimeUtil.calculateTimespanInSeconds(
+				new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,32)), 
+				new QDateTime(new QDate(2011, 3, 5), new QTime(23,3,32))));
 	}
 	
 	@Test
 	public void calculateTimespanDayDifferenceNoOverflow() {
-		assertEquals(2*24*60*60, TimeUtil.calculateTimespanInSeconds(new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,32)), new QDateTime(new QDate(2011, 3, 7), new QTime(12,3,32))));
+		assertEquals(2*24*60*60, TimeUtil.calculateTimespanInSeconds(
+				new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,32)), 
+				new QDateTime(new QDate(2011, 3, 7), new QTime(12,3,32))));
 	}
 	
 	@Test
-	public void calculateTimespanMonthDifference() {
-		
+	public void calculateTimespanMonthDifferenceNoLeapyear() {
+		assertEquals(28*24*60*60, TimeUtil.calculateTimespanInSeconds(
+				new QDateTime(new QDate(2011, 2, 5), new QTime(12,3,32)), 
+				new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,32))));
+	}
+	
+	@Test
+	public void calculateTimespanMonthDifferenceLeapyear() {
+		assertEquals(29*24*60*60, TimeUtil.calculateTimespanInSeconds(
+				new QDateTime(new QDate(2008, 2, 5), new QTime(12,3,32)), 
+				new QDateTime(new QDate(2008, 3, 5), new QTime(12,3,32))));
+	}
+	
+	@Test
+	public void calculateTimespanYearDifferenceNoLeapyear() {
+		assertEquals((7*31+4*30+28)*24*60*60, TimeUtil.calculateTimespanInSeconds(
+				new QDateTime(new QDate(2010, 3, 5), new QTime(12,3,32)), 
+				new QDateTime(new QDate(2011, 3, 5), new QTime(12,3,32))));
+	}
+	
+	@Test
+	public void calculateTimespanYearDifferenceLeapyear() {
+		assertEquals((7*31+4*30+29)*24*60*60, TimeUtil.calculateTimespanInSeconds(
+				new QDateTime(new QDate(2008, 1, 5), new QTime(12,3,32)), 
+				new QDateTime(new QDate(2009, 1, 5), new QTime(12,3,32))));
 	}
 
 //TODO BeforeTime is after afterTime
