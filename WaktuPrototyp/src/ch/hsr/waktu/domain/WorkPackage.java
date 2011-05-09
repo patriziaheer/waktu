ce99a87827fb92e849c2590d46d4699167d917c3
Mike <mfisler@hsr.ch>
2011-05-09 06:31:17 +0200

    WorkPackage: equals(), hashCode() added

--- WaktuPrototyp/src/ch/hsr/waktu/domain/WorkPackage.java
+++ WaktuPrototyp/src/ch/hsr/waktu/domain/WorkPackage.java
@@ -52,4 +52,27 @@ public class WorkPackage {
 	public String toString() {
 		return description;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof WorkPackage) {
+			WorkPackage wp = (WorkPackage)obj;
+			if (wp.id == id &&
+			wp.description.equals(description) &&
+			wp.active == active &&
+			wp.project.equals(project)) {
+				return true;
+			}
+		}
+		return super.equals(obj);
+	}
+	
+	@Override
+	public int hashCode() {
+		int hashCode = 23;
+		hashCode = 31 * id;
+		hashCode = 31 * description.hashCode();
+		hashCode = 31 * project.hashCode();
+		return hashCode;
+	}
 }
