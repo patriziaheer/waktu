8d7993e7ed682ffc427251217d87c56e22effe66
Mike <mfisler@hsr.ch>
2011-05-09 06:50:11 +0200

    ProjectStaff: equals(), hashCode() added

--- WaktuPrototyp/src/ch/hsr/waktu/domain/ProjectStaff.java
+++ WaktuPrototyp/src/ch/hsr/waktu/domain/ProjectStaff.java
@@ -40,4 +40,25 @@ public class ProjectStaff {
 		this.user = user;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof ProjectStaff) {
+			ProjectStaff ps = (ProjectStaff)obj;
+			if (ps.id == id &&
+			ps.project.equals(project) &&
+			ps.user.equals(user)) {
+				return true;
+			}
+		}
+		return super.equals(obj);
+	}
+	
+	@Override
+	public int hashCode() {
+		int hashCode = 23;
+		hashCode += 31 * id;
+		hashCode += 31 * project.hashCode();
+		hashCode += 31 * user.hashCode();
+		return hashCode;
+	}
 }
