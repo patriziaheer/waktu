b9d2ab5eebeba1158f22992de9fe79ad1ecf1f88
Mike <mfisler@hsr.ch>
2011-05-11 15:39:29 +0200

    UsernameUtil and TestUsernameUtil

--- WaktuPrototyp/src/ch/hsr/waktu/services/UsernameUtil.java
+++ WaktuPrototyp/src/ch/hsr/waktu/services/UsernameUtil.java
@@ -9,7 +9,7 @@ import ch.hsr.waktu.domain.Usr;
 public class UsernameUtil {
 	
 	public static String generateUsername(List<Usr> userList, String firstname, String lastname) throws WaktuGeneralException {
-		String username = firstname.toLowerCase() + lastname.toLowerCase().replaceAll(" ", "");
+		String username = firstname.toLowerCase().replaceAll(" ", "") + lastname.toLowerCase().replaceAll(" ", "");
 		int usernameOccurrence = 0;
 		for(Usr u: userList) {
 			if(u.getUsername().startsWith(username)) {
