e02e1443c427006a05e17a3cf19441fbdbfb7386
unknown <pheer@.scs-ad.scs.ch>
2011-05-12 14:44:26 +0200

    Style

---
+++ WaktuPrototyp/config/ProjectDetails_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>.ProjectDetails</name>
+    <message>
+        <location filename="../src/ch/hsr/waktu/gui/qt/view/ProjectDetails.java" line="132"/>
+        <source>Close</source>
+        <translation>Close</translation>
+    </message>
+</context>
+</TS>
