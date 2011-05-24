bf2574fcdf1f3c392331ffa7f07a9337e3883462
unknown <pheer-mobile@.scs-ad.scs.ch>
2011-05-24 11:54:13 +0200

    Translation

---
+++ WaktuPrototyp/config/w-de.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_CH">
+<context>
+    <name>Calendar</name>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="189"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="190"/>
+        <source>Mon</source>
+        <translation>Mo</translation>
+    </message>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="191"/>
+        <source>Wed</source>
+        <translation>Mi</translation>
+    </message>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="192"/>
+        <source>Fri</source>
+        <translation>Fri</translation>
+    </message>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="193"/>
+        <source>Sun</source>
+        <translation>So</translation>
+    </message>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="194"/>
+        <source>Sat</source>
+        <translation>Sa</translation>
+    </message>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="195"/>
+        <source>Thu</source>
+        <translation>Do</translation>
+    </message>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="196"/>
+        <source>Tue</source>
+        <translation>Di</translation>
+    </message>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="197"/>
+        <source>-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="198"/>
+        <source>&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Generated JUIC files/ch/hsr/waktu/gui/qt/view/Ui_Calendar.java" line="199"/>
+        <source>&lt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_CH">
+<context>
+    <name>FavoriteModel</name>
+    <message>
+        <location filename="../src/ch/hsr/waktu/gui/qt/model/FavoriteModel.java" line="71"/>
+        <source>Workpackage</source>
+        <translation>Arbeitspaket</translation>
+    </message>
+    <message>
+        <location filename="../src/ch/hsr/waktu/gui/qt/model/FavoriteModel.java" line="73"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../src/ch/hsr/waktu/gui/qt/model/FavoriteModel.java" line="75"/>
+        <source>End</source>
+        <translation>End</translation>
+    </message>
+</context>
+</TS>
+
