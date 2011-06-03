ed3f8984919fa8bc7203bd774d671807b2b4515b
sstaehel <sstaehel@hsr.ch>
2011-06-03 12:09:42 +0200

    final

---
+++ WaktuPrototyp/src/ch/hsr/waktu/controller/datacontroller/classWorkPackageController.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once '../../domain/class.WorkPackage.php';
+
+class WorkPackageController {
+
+	private static  $instance = null;
+	private $con;
+	
+	static public function getInstance() {
+		if(null === self::$instance) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+	
+	public function __construct() {
+		$this->con = PersistenceController::getInstance()->connection;
+	}
+	
+	public function getAllWorkPackage() {
+
+		$query = pg_query($this->con, "SELECT * FROM workpackage");
+		
+		$counter = 0;
+		
+		while($data = pg_fetch_object($query)) {
+			$workPackages[$counter] = new WorkPackage($description, $active, $project);
+			$counter++;
+		}
+		
+		return $workPackages;
+	}
+	
+	public function getWorkPackage($id) {
+
+		$query = pg_query($this->con, "SELECT * FROM workpackage WHERE workpackageid = ".$id);
+		
+		while($data = pg_fetch_object($query)) {
+			$workPackage = new WorkPackage($description, $active, $project);
+		}
+		
+		return $workPackage;		
+	}
+	
+	
+	
+}
